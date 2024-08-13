c09fa20510dedfe7d065e40837464c11494ecae4
YASMINCOS <costayasmin367@gmail.com>
2024-08-13 15:27:30 -0300

    create TransactionRepository

---
+++ src/main/java/com/picpay/picpay/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.picpay.picpay.repositories;
+
+import com.picpay.picpay.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+
+}
